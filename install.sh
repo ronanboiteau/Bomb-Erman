e44e3f0494db90282f413a45ee38986a192a1cdb
Erwan Simon <GladToBeInsane@users.noreply.github.com>
2017-09-19 09:40:57 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,1 @@
-cd bin && cmake .. && sudo make install && make clean && rm -rf ./* && cd .. && echo "*** The installation was successful, run indie_studio to start the game ! ***"
+cd bin && cmake .. && sudo make install && make clean && rm -rf ./* && cd .. && echo "*** The installation was successful, run indie_studio to start the game !! ***"
